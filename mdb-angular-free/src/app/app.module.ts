dd592f972de8ac483d08fdafa0a00a5343e48aa5
joaokage1 <j_vgomes@hotmail.com>
2020-03-15 16:20:22 -0300

    initial commit

---
+++ mdb-angular-free/src/app/app.module.ts
@@ -0,0 +1,46 @@
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+
+import { AppComponent } from './app.component';
+
+import { MDBBootstrapModule } from 'angular-bootstrap-md';
+import { FormsModule } from '@angular/forms';
+import { LoginUserComponent } from './components/login-user/login-user.component';
+import { RegisterUserComponent } from './components/register-user/register-user.component';
+import { ResendRegistrationTokenComponent } from './components/resend-registration-token/resend-registration-token.component';
+import { EditUserComponent } from './components/edit-user/edit-user.component';
+import { ListUserComponent } from './components/list-user/list-user.component';
+import { WelcomeComponent } from './components/welcome/welcome.component';
+import { HeaderComponent } from './shared/components/navigation/header/header.component';
+import { DeleteUserModalComponent } from './shared/components/modals/delete-user-modal/delete-user-modal.component';
+import { RouterModule } from '@angular/router';
+import { AppRoutingModule } from './app-routing.module';
+import { FooterComponent } from './shared/components/navigation/footer/footer.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    LoginUserComponent,
+    RegisterUserComponent,
+    ResendRegistrationTokenComponent,
+    EditUserComponent,
+    ListUserComponent,
+    WelcomeComponent,
+    HeaderComponent,
+    DeleteUserModalComponent,
+    FooterComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    MDBBootstrapModule.forRoot(),
+    RouterModule,
+    AppRoutingModule,
+    FormsModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
---
+++ mdb-angular-free/src/app/shared/comum/app.utils.ts
@@ -0,0 +1,19 @@
+import { HttpHeaders } from '@angular/common/http';
+export const BASE_URL = 'https://hackatoddy.herokuapp.com';
+export const URL_TOKEN = BASE_URL + 'oauth/token';
+export const REGISTER_URL = BASE_URL + 'api/public/registration/users';
+export const CONFIRM_REGISTER_URL = BASE_URL + 'api/public/regitrationConfirm/users';
+export const RESEND_REGISTER_TOKEN_URL = BASE_URL + 'api/public/resendRegistrationToken/users';
+const headersToken = new HttpHeaders({
+    Authorization: 'Bearer ' + window.localStorage.getItem('accessToken')
+});
+export const OPTIONS_OBJECTO = { headers: headersToken };
+export const HEADERS_LOGIN = new HttpHeaders({
+    Authorization: 'Basic ' + btoa('client' + ':' + '123')
+});
+export const HEADERS_REGISTER = new HttpHeaders({
+    Authorization: 'Basic ' + btoa('client' + ':' + '123')
+});
+export const HEADERS_COMMUN = new HttpHeaders({
+    Authorization: 'Basic ' + btoa('client' + ':' + '123')
+});
